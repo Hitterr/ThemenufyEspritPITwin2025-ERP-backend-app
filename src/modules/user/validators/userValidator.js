655cdc8654c26dcd642b87da792d6eefa0beb157
OussemaJridi <oussemajridi78@gmail.com>
2025-03-05 00:15:28 +0100

    Update userValidator.js

--- src/modules/user/validators/userValidator.js
+++ src/modules/user/validators/userValidator.js
@@ -1,19 +1,28 @@
 const yup = require("yup");
 
 const userSchema = yup.object({
-  name: yup.string().required("Name is required"),
-  email: yup.string().email("Invalid email").required("Email is required"),
+  firstName: yup.string().required("First name is required").trim(),
+  lastName: yup.string().required("Last name is required").trim(),
+  email: yup
+    .string()
+    .email("Invalid email")
+    .required("Email is required")
+    .trim()
+    .lowercase(),
   password: yup
     .string()
     .min(6, "Password must be at least 6 characters")
     .required("Password is required"),
+  image: yup.string().nullable(),
+  phone: yup.number().nullable(),
+  address: yup.string().nullable(),
+  birthday: yup.string().nullable(),
   role: yup
     .string()
-    .oneOf(["user", "admin", "superadmin", "employee", "client"])
-    .default("user"),
+    .oneOf(["admin", "superadmin", "employee", "client"])
+    .default("admin"),
   isEmailVerified: yup.boolean().default(false),
   verifiedDevices: yup.array().of(yup.string()).default([]),
-  profilePicture: yup.string().nullable(),
   authProvider: yup
     .string()
     .oneOf(["local", "google", "facebook"])
