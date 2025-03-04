36b9d274de37e4f89038125d1f2f0ce907a87750
BEN SALEM EBTIHEL <157884923+ebtihel1@users.noreply.github.com>
2025-03-04 23:21:23 +0100

    crudRestaurant

--- src/server.js
+++ src/server.js
@@ -21,6 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/auth", require("@modules/auth/routes"));
 app.use("/api/user", require("@modules/user/routes"));
+app.use("/api/restaurant", require("@modules/restaurant/routes"));
 // Start server
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
