329e43f10b4ab2a5a849b84344ca034f0ec34a6c
lolly1259998 <hadilbouhachem12@gmail.com>
2025-03-04 22:37:53 +0100

    Crud Employee

--- src/server.js
+++ src/server.js
@@ -21,6 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/auth", require("@modules/auth/routes"));
 app.use("/api/user", require("@modules/user/routes"));
+app.use("/api/employee", require("@modules/employee"));
 // Start server
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
