26b1b109bdf2c9aacfb9d53f290909ccbcdd9698
BEN SALEM EBTIHEL <157884923+ebtihel1@users.noreply.github.com>
2025-03-05 01:04:09 +0100

    signUP

--- src/modules/auth/routes/index.js
+++ src/modules/auth/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const loginRoutes = require("./loginRoutes");
 const deviceRoutes = require("./deviceRoutes");
+const signupRoutes = require("./signupRoutes");
 
 router.use("/login", loginRoutes);
 router.use("/devices", deviceRoutes);
+router.use("/signup", signupRoutes);
 module.exports = router;
