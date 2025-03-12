2134abbea8acb25ba397fa134111aaf3a243a82f
lolly1259998 <hadilbouhachem12@gmail.com>
2025-03-12 09:03:16 +0100

    Squashed commit of the following:
    commit 09ce15fd3e7b2e3b97b5c5b34522473a3161aca1
    Author: OussemaJridi <oussemajridi78@gmail.com>
    Date:   Wed Mar 12 03:27:31 2025 +0100
    
        signup
    
    commit 6aa857a767cbd42505a7f192761ebb73d39715cf
    Author: OussemaJridi <oussemajridi78@gmail.com>
    Date:   Wed Mar 12 00:13:56 2025 +0100
    
        update google auth service

--- src/modules/user/validators/userValidator.js
+++ src/modules/user/validators/userValidator.js
@@ -1,34 +1,28 @@
 const yup = require("yup");
-
 const userSchema = yup.object({
-  firstName: yup.string().required("First name is required").trim(),
-  lastName: yup.string().required("Last name is required").trim(),
-  email: yup
-    .string()
-    .email("Invalid email")
-    .required("Email is required")
-    .trim()
-    .lowercase(),
-  password: yup
-    .string()
-    .min(6, "Password must be at least 6 characters")
-    .required("Password is required"),
-  image: yup.string().nullable(),
-  phone: yup.string().nullable(),
-  address: yup.string().nullable(),
-  birthday: yup.string().nullable(),
-  role: yup
-    .string()
-    .oneOf(["admin", "superadmin", "employee", "client"])
-    .default("admin"),
-  isEmailVerified: yup.boolean().default(false),
-  verifiedDevices: yup.array().of(yup.string()).default([]),
-  authProvider: yup
-    .string()
-    .oneOf(["local", "google", "facebook"])
-    .default("local"),
+	firstName: yup.string().required("First name is required").trim(),
+	lastName: yup.string().required("Last name is required").trim(),
+	email: yup
+		.string()
+		.email("Invalid email")
+		.required("Email is required")
+		.trim()
+		.lowercase(),
+	password: yup
+		.string()
+		.min(6, "Password must be at least 6 characters")
+		.required("Password is required"),
+	image: yup.string().nullable(),
+	phone: yup.string().nullable(),
+	address: yup.string().min(3).nullable(),
+	birthday: yup.date().nullable(),
+	isEmailVerified: yup.boolean().default(false),
+	verifiedDevices: yup.array().of(yup.string()).default([]),
+	authProvider: yup
+		.string()
+		.oneOf(["local", "google", "facebook"])
+		.default("local"),
 });
-
 module.exports = {
-  userSchema,
+	userSchema,
 };
