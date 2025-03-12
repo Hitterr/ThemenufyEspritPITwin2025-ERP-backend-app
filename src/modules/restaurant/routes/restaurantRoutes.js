245efb429a0c0f648aa7a10f954fe88835366893
lolly1259998 <hadilbouhachem12@gmail.com>
2025-03-12 09:03:33 +0100

    Squashed commit of the following:
    commit 2134abbea8acb25ba397fa134111aaf3a243a82f
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Wed Mar 12 09:03:16 2025 +0100
    
        Squashed commit of the following:
    
        commit 09ce15fd3e7b2e3b97b5c5b34522473a3161aca1
        Author: OussemaJridi <oussemajridi78@gmail.com>
        Date:   Wed Mar 12 03:27:31 2025 +0100
    
            signup
    
        commit 6aa857a767cbd42505a7f192761ebb73d39715cf
        Author: OussemaJridi <oussemajridi78@gmail.com>
        Date:   Wed Mar 12 00:13:56 2025 +0100
    
            update google auth service
    
    commit 281c549976c236511a09150f7e36f179c9339059
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Wed Mar 12 09:02:49 2025 +0100
    
        fix profile update password routes
    
    commit c8984214c01198a9a0c5c8ed0fd3102d88ecd663
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Sun Mar 9 05:28:34 2025 +0100
    
        update resto && password
    
    commit 96c58c0f0e8db481a8e3f622a7801b9506cf2f35
    Merge: 18d6b2a c21a09c
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Sun Mar 9 02:17:29 2025 +0100
    
        Merge branch 'main' into 2025/ERP/Profile
    
    commit 18d6b2ac13643078248740b09064a415753d6295
    Merge: dee84aa b011b1f
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Wed Mar 5 04:22:14 2025 +0100
    
        Merge branch 'main' into 2025/ERP/Profile
    
    commit dee84aaadc51c2951297dd7627376b17beb6e75f
    Merge: ac4e804 9586b78
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Wed Mar 5 04:14:32 2025 +0100
    
        Merge branch 'main' into 2025/ERP/Profile
    
    commit ac4e804baf3cf93a8c9fbbdd6d1a36ce6682174b
    Merge: 7f44c62 346a0d4
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Wed Mar 5 04:12:29 2025 +0100
    
        Merge branch 'main' into 2025/ERP/Profile
    
    commit 7f44c62ffa02c5131597cceff80fc7e243c1fd37
    Merge: c4b323c 97a4cf9
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Wed Mar 5 04:11:09 2025 +0100
    
        Merge branch 'main' into 2025/ERP/Profile
    
    commit c4b323c5d38c2214431d338d271c91ab496006b1
    Merge: 1818b1d 84b78d6
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Wed Mar 5 03:56:32 2025 +0100
    
        Merge branch 'main' into 2025/ERP/Profile
    
    commit 1818b1d685c8d24ff4f34c543848d0d8aa40de6f
    Merge: 922f1c0 a02915b
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Wed Mar 5 03:43:19 2025 +0100
    
        Merge branch 'main' into 2025/ERP/Profile
    
    commit 922f1c0c9de2e5388b2cf6d67b9f28e5168a2c7a
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Wed Mar 5 03:30:10 2025 +0100
    
        profile update done !
    
    commit cf4ba1686093a9b65190dcd16ce6668d2310a7c7
    Merge: 329e43f b28140d
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Wed Mar 5 01:22:32 2025 +0100
    
        Merge branch 'main' into 2025/ERP/Profile
    
    commit 329e43f10b4ab2a5a849b84344ca034f0ec34a6c
    Author: lolly1259998 <hadilbouhachem12@gmail.com>
    Date:   Tue Mar 4 22:37:53 2025 +0100
    
        Crud Employee

--- src/modules/restaurant/routes/restaurantRoutes.js
+++ src/modules/restaurant/routes/restaurantRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const restaurantController = require("../controllers/restaurantController");
+const authMiddleware = require("@middlewares/authMiddleware");
+const restaurantOwnerCheck = require("../../../middlewares/restaurantOwnerCheck");
 // const { authenticateToken } = require("../middleware/auth");
 
 // Routes publiques
@@ -10,7 +12,17 @@ router.post("/", restaurantController.createRestaurant);
 // router.use(authenticateToken);
 router.get("/", restaurantController.getAllRestaurants);
 router.get("/:id", restaurantController.getRestaurantById);
-router.put("/:id", restaurantController.updateRestaurant);
-router.delete("/:id", restaurantController.deleteRestaurant);
+router.put(
+  "/:id",
+  authMiddleware.verifyToken,
+  restaurantOwnerCheck,
+  restaurantController.updateRestaurant
+);
+router.delete(
+  "/:id",
+  authMiddleware.verifyToken,
+  restaurantOwnerCheck,
+  restaurantController.deleteRestaurant
+);
 
 module.exports = router;
