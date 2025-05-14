2e857092bf13c79c2c8d410032e09c85421eaefb
OussemaJridi <oussemajridi78@gmail.com>
2025-05-14 01:09:47 +0100

    category middleware

--- src/modules/category/controllers/category.controller.js
+++ src/modules/category/controllers/category.controller.js
@@ -1,101 +1,122 @@
 const categoryService = require("../services/category.service");
 const createCategory = async (req, res) => {
-	try {
-		const category = await categoryService.createCategory(req.body);
-		res.status(201).json(category);
-	} catch (err) {
-		res.status(400).json({ error: err.message });
-	}
+  try {
+    const restaurantId = req.user.details.restaurant._id;
+    req.body.restaurant = restaurantId;
+    const category = await categoryService.createCategory(req.body);
+    res.status(201).json(category);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 const getCategories = async (req, res) => {
-	try {
-		const page = parseInt(req.query.page) || 1;
-		const limit = parseInt(req.query.limit) || 10;
-		const result = await categoryService.getCategories(page, limit);
-		res.json({
-			success: true,
-			data: result.categories,
-			pagination: {
-				currentPage: result.currentPage,
-				totalPages: result.totalPages,
-				totalItems: result.totalItems,
-				limit: limit,
-			},
-		});
-	} catch (err) {
-		res.status(500).json({ error: err.message });
-	}
+  try {
+    req.query.restaurant = req.user.details.restaurant._id;
+    const filters = req.query || {};
+    const result = await categoryService.getCategories(filters);
+    res.json({
+      success: true,
+      data: result.categories,
+      pagination: {
+        currentPage: result.currentPage,
+        totalPages: result.totalPages,
+        totalItems: result.totalItems,
+        limit: req.query.limit,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 const getCategoryById = async (req, res) => {
-	try {
-		const category = await categoryService.getCategoryById(req.params.id);
-		if (!category) return res.status(404).json({ error: "Category not found" });
-		res.json(category);
-	} catch (err) {
-		res.status(500).json({ error: err.message });
-	}
+  try {
+    const restaurantId = req.user.details.restaurant._id;
+    const category = await categoryService.getCategoryById(
+      req.params.id,
+      restaurantId
+    );
+    if (!category) return res.status(404).json({ error: "Category not found" });
+    if (
+      category.restaurant.toString() !==
+      req.user.details.restaurant._id.toString()
+    ) {
+      return res.status(403).json({ error: "Forbidden" });
+    }
+    res.json(category);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 const updateCategory = async (req, res) => {
-	try {
-		const category = await categoryService.updateCategory(
-			req.params.id,
-			req.body
-		);
-		if (!category) return res.status(404).json({ error: "Category not found" });
-		res.json(category);
-	} catch (err) {
-		res.status(400).json({ error: err.message });
-	}
+  try {
+    const restaurantId = req.user.details.restaurant._id;
+
+    const category = await categoryService.updateCategory(
+      req.params.id,
+      req.body,
+      restaurantId
+    );
+    if (!category) return res.status(404).json({ error: "Category not found" });
+    res.json(category);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 const deleteCategory = async (req, res) => {
-	try {
-		const category = await categoryService.deleteCategory(req.params.id);
-		if (!category) return res.status(404).json({ error: "Category not found" });
-		res.json({ message: "Category deleted" });
-	} catch (err) {
-		res.status(500).json({ error: err.message });
-	}
+  try {
+    const restaurantId = req.user.details.restaurant._id;
+    const category = await categoryService.deleteCategory(
+      req.params.id,
+      restaurantId
+    );
+    if (!category) return res.status(404).json({ error: "Category not found" });
+    res.json({ message: "Category deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 const addSubCategory = async (req, res) => {
-	try {
-		const { categoryId } = req.body;
-		if (req.body.name && req.body.description) {
-			const subCategory = await categoryService.createCategory({
-				name: req.body.name,
-				description: req.body.description,
-			});
-			req.body.subCategoryId = subCategory._id;
-		}
-		const category = await categoryService.addSubCategory(
-			categoryId,
-			req.body.subCategoryId
-		);
-		if (!category) return res.status(404).json({ error: "Category not found" });
-		res.json(category);
-	} catch (err) {
-		res.status(400).json({ error: err.message });
-	}
+  try {
+    const { categoryId } = req.body;
+    if (req.body.name && req.body.description) {
+      req.body.restaurant = req.user.details.restaurant._id;
+      const subCategory = await categoryService.createCategory({
+        name: req.body.name,
+        description: req.body.description,
+        restaurant: req.user.details.restaurant._id,
+      });
+      req.body.subCategoryId = subCategory._id;
+    }
+    const category = await categoryService.addSubCategory(
+      categoryId,
+      req.body.subCategoryId
+    );
+    if (!category) return res.status(404).json({ error: "Category not found" });
+    res.json(category);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 const removeSubCategory = async (req, res) => {
-	try {
-		const { categoryId, subCategoryId } = req.body;
-		console.log("📢 [category.controller.js:75]", categoryId, subCategoryId);
-		const category = await categoryService.removeSubCategory(
-			categoryId,
-			subCategoryId
-		);
-		if (!category) return res.status(404).json({ error: "Category not found" });
-		res.json(category);
-	} catch (err) {
-		res.status(400).json({ error: err.message });
-	}
+  try {
+    const { categoryId, subCategoryId } = req.body;
+    console.log("📢 [category.controller.js:75]", categoryId, subCategoryId);
+    const category = await categoryService.removeSubCategory(
+      categoryId,
+      subCategoryId
+    );
+    if (!category) return res.status(404).json({ error: "Category not found" });
+    res.json(category);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 module.exports = {
-	createCategory,
-	getCategories,
-	getCategoryById,
-	updateCategory,
-	deleteCategory,
-	addSubCategory,
-	removeSubCategory,
+  createCategory,
+  getCategories,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
+  addSubCategory,
+  removeSubCategory,
 };
--- src/modules/category/services/category.service.js
+++ src/modules/category/services/category.service.js
@@ -1,68 +1,91 @@
 const Category = require("../../../models/category");
 const createCategory = async (data) => {
-	return await Category.create(data);
+  return await Category.create(data);
 };
-const getCategories = async (page = 1, limit = 10) => {
-	const skip = (page - 1) * limit;
-	const categories = await Category.find().skip(skip).limit(limit);
-	const total = await Category.countDocuments();
-	return {
-		categories,
-		currentPage: page,
-		totalPages: Math.ceil(total / limit),
-		totalItems: total,
-	};
+const getCategories = async (filters = { page: 1, limit: 10 }) => {
+  try {
+    const query = buildFiltersQuery(filters);
+    const page = parseInt(filters.page, 10) || 1; // Convertir la chaîne en nombre entier
+    const limit = parseInt(filters.limit, 10) || 10; // Convertir la chaîne en nombre entier
+    // ================================================
+    const skip = (page - 1) * limit;
+    const categories = await Category.find(query).skip(skip).limit(limit);
+    const total = await Category.countDocuments();
+    return {
+      categories,
+      currentPage: page,
+      totalPages: Math.ceil(total / limit),
+      totalItems: total,
+    };
+  } catch (error) {
+    console.error(error);
+  }
 };
-const getCategoryById = async (id) => {
-	return await Category.findById(id).populate("subCategories");
+const getCategoryById = async (id, restaurant) => {
+  return await Category.findOne({ _id: id, restaurant }).populate(
+    "subCategories"
+  );
 };
-const updateCategory = async (id, data) => {
-	return await Category.findByIdAndUpdate(id, data, { new: true });
+const updateCategory = async (id, data, restaurant) => {
+  return await Category.findOneAndUpdate({ _id: id, restaurant }, data, {
+    new: true,
+  });
 };
-const deleteCategory = async (id) => {
-	return await Category.findByIdAndDelete(id);
+const deleteCategory = async (id, restaurant) => {
+  return await Category.findOneAndDelete({ _id: id, restaurant });
 };
 const addSubCategory = async (categoryId, subCategoryId) => {
-	const category = await Category.findById(categoryId);
-	if (!category) {
-		throw new Error("Category not found");
-	}
-	const subCategory = await Category.findById(subCategoryId);
-	if (!subCategory) {
-		throw new Error("Subcategory not found");
-	}
-	if (category.subCategories.includes(subCategoryId)) {
-		throw new Error("Subcategory already exists in this category");
-	}
-	category.subCategories.push(subCategoryId);
-	await category.save();
-	return category;
+  const category = await Category.findById(categoryId);
+  if (!category) {
+    throw new Error("Category not found");
+  }
+  const subCategory = await Category.findById(subCategoryId);
+  if (!subCategory) {
+    throw new Error("Subcategory not found");
+  }
+  if (category.subCategories.includes(subCategoryId)) {
+    throw new Error("Subcategory already exists in this category");
+  }
+  category.subCategories.push(subCategoryId);
+  await category.save();
+  return category;
 };
 const removeSubCategory = async (categoryId, subCategoryId) => {
-	const category = await Category.findById(categoryId);
-	if (!category) {
-		throw new Error("Category not found");
-	}
-	const subCategory = await Category.findById(subCategoryId);
-	if (!subCategory) {
-		throw new Error("Subcategory not found");
-	}
-	if (!category.subCategories.includes(subCategoryId)) {
-		throw new Error("Subcategory does not exist in this category");
-	}
-	console.log("Subcategory ID:", subCategoryId);
-	category.subCategories = category.subCategories.filter(
-		(id) => id.toString() !== subCategoryId
-	);
-	await category.save();
-	return category;
+  const category = await Category.findById(categoryId);
+  if (!category) {
+    throw new Error("Category not found");
+  }
+  const subCategory = await Category.findById(subCategoryId);
+  if (!subCategory) {
+    throw new Error("Subcategory not found");
+  }
+  if (!category.subCategories.includes(subCategoryId)) {
+    throw new Error("Subcategory does not exist in this category");
+  }
+  console.log("Subcategory ID:", subCategoryId);
+  category.subCategories = category.subCategories.filter(
+    (id) => id.toString() !== subCategoryId
+  );
+  await category.save();
+  return category;
+};
+const buildFiltersQuery = (filters) => {
+  const query = {};
+  if (filters?.search) {
+    query.name = { $regex: filters?.search, $options: "i" };
+  }
+  if (filters?.restaurant) {
+    query.restaurant = filters?.restaurant;
+  }
+  return query;
 };
 module.exports = {
-	createCategory,
-	getCategories,
-	getCategoryById,
-	updateCategory,
-	deleteCategory,
-	addSubCategory,
-	removeSubCategory,
+  createCategory,
+  getCategories,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
+  addSubCategory,
+  removeSubCategory,
+  buildFiltersQuery,
 };
